2ede442a66590f33bbda26feb369652b5ad68e18
SIDDHARTH <siddharthsinghjaipur2001@gmail.com>
2023-04-02 00:33:31 +0530

    Cors

--- server/app.js
+++ server/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const cors = require('cors')
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
-  next();
-});
+app.use(cors())
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+//   next();
+// });
 
 dotenv.config({ path: "./.env" });
 const port = process.env.PORT || 5399;
