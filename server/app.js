50e2d70bdb8f89398d3432a7f5b2fc25d47bd5cf
SIDDHARTH <siddharthsinghjaipur2001@gmail.com>
2023-04-02 00:27:35 +0530

    Fix

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
   next();
 });
 
