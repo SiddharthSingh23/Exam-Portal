c69ab2ab9271a29d3813b51f868cd18ecfd4fabf
SID <67636770+SiddharthSingh23@users.noreply.github.com>
2023-04-06 16:18:39 +0530

    Test

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,6 +7,10 @@ const userController = require("../controller/userController");
 const { createUserMiddleware, loginUserMiddleware } = userMiddleware;
 const { createUser, loginUser } = userController;
 
+app.get("/get", async (req, res) => {
+    res.send("Hello") 
+});
+
 app.post("/signup", createUserMiddleware, createUser);
 app.post("/login", loginUserMiddleware, loginUser);
 
